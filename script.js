2647dd67c77b5371d90450d253368e92ba425dcf
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-07 20:30:56 +0100

    sum function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.getElementById("calculate").addEventListener("click", calculate);
 
 function calculate() {
     let value1 = document.getElementById("firstnumber").value;
-    let value2 = document.getElementById("secondnumber").value;
+    let value2 = document.getElementById("secondnumber").value; }
+    
+    let sum = parseInt(value1) + parseInt(value2);
 
 
