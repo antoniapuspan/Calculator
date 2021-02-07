30f86963f02f7d6efdac71a620f8995e84c1872d
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-07 20:29:58 +0100

    function calculate

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById("calculate").addEventListener("click", calculate);
 
+function calculate() {
+    let value1 = document.getElementById("firstnumber").value;
+    let value2 = document.getElementById("secondnumber").value;
+
 
