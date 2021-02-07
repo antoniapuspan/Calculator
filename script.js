2493e79fb95ac36afdc21564b0dd684b2b281f5f
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-07 20:47:16 +0100

    all operators working

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ document.getElementById("calculate").addEventListener("click", calculate);
 
 function calculate() {
     let value1 = document.getElementById("firstnumber").value;
-    let value2 = document.getElementById("secondnumber").value; 
+    let value2 = document.getElementById("secondnumber").value;
 
-    let sum = parseInt(value1) + parseInt(value2);
+    let operatorValue = document.getElementById("operator").value;
+    let result;
 
-    document.getElementById("firstnumber").value = sum;
+    if (operatorValue == "add") {
+        result = parseInt(value1) + parseInt(value2);
+    } else if (operatorValue == "sub") {
+        result = parseInt(value1) - parseInt(value2);
+    } else if (operatorValue == "mul") {
+        result = parseInt(value1) * parseInt(value2);
+    } else {
+        result = parseInt(value1) / parseInt(value2);
+    }
 
-    let operator = document.getElementById("operator").value;
-    console.log(operator);
+
+
+    document.getElementById("firstnumber").value = result;
+
+
 
 
 }
