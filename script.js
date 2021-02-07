8131d2781a869ae195b872156092a164fbefa01a
antoniapuspan <65085166+antoniapuspan@users.noreply.github.com>
2021-02-07 20:32:07 +0100

    writing in input fields + Write the code so it reads the two numbers, add them together, and writes the result in the first input-field

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ document.getElementById("calculate").addEventListener("click", calculate);
 
 function calculate() {
     let value1 = document.getElementById("firstnumber").value;
-    let value2 = document.getElementById("secondnumber").value; }
-    
+    let value2 = document.getElementById("secondnumber").value; 
+
     let sum = parseInt(value1) + parseInt(value2);
 
+    document.getElementById("firstnumber").value = sum;
 
+
+}
